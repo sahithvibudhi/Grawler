c2151bdb5db4cec50474f4cc504a1e9a7c08f971
sahithvibudhi <v.sahithkumar@gmail.com>
2017-12-22 18:34:46 +0530

    first request success

--- Requests.go
+++ Requests.go
@@ -1,3 +1,20 @@
 package main
 
+import (
+	"net/http"
+	"fmt"
+)
 
+func get(link string) {
+
+	resp, err := http.Get(link)
+
+	if err != nil {
+		panic(err)
+	}
+
+	defer resp.Body.Close()
+
+	var res string = formatResponse(resp)
+	fmt.Println(res)
+}
--- helpers.go
+++ helpers.go
@@ -1,13 +1,33 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+	"io/ioutil"
+	"net/http"
+)
 
 func Init() {
-
+	get("http://naasongs.com")
 }
 
 func LoadConfig(conf string) {
 
 	fmt.Println(conf)
 
+}
+
+func formatResponse(resp *http.Response) string {
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		panic(err)
+	}
+	return string(body)
+	
+}
+
+func keepLines(s string, n int) string {
+	result := strings.Join(strings.Split(s, "\n")[:n], "\n")
+	return strings.Replace(result, "\r", "", -1)
 }
